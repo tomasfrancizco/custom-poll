b0fe692ee0a1c0f054dce50ac7d346d79e26757e
Tomas <tomasfreire@gmail.com>
2020-07-09 18:51:51 -0300

    simple night mode

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const body = document.getElementsByTagName("body")[0];
 const optionA = document.getElementById("option-a");
 const optionB = document.getElementById("option-b");
 const optionC = document.getElementById("option-c");
 const optionD = document.getElementById("option-d");
-const button = document.getElementById("submit-btn");
+const submitButton = document.getElementById("submit-btn");
+const nightButton = document.getElementsByClassName("night-mode-btn");
 
 const resultA = document.getElementById("result-a");
 const resultB = document.getElementById("result-b");
@@ -11,10 +13,9 @@ const resultD = document.getElementById("result-d");
 
 const voters = document.getElementById("voters");
 
-// console.log(Date.now())
 let options = [0, 0, 0, 0];
 
-button.onclick = () => {
+submitButton.onclick = () => {
   if(optionA.checked){
     options[0]++
   } else if(optionB.checked){
@@ -28,14 +29,21 @@ button.onclick = () => {
   }
   const total = options.reduce((acc, val) => acc+val);
   resultA.style.width = `${options[0] * 100 / total}%`;
-  resultA.innerHTML = `${(Number(options[0])*100/Number(total)).toFixed(0)}%`
+  resultA.innerHTML = `${Math.round((Number(options[0])*100/Number(total)))}%`
   resultB.style.width = `${options[1] * 100 / total}%`;
-  resultB.innerHTML = `${(Number(options[1])*100/Number(total)).toFixed(0)}%`
+  resultB.innerHTML = `${Math.round((Number(options[1])*100/Number(total)))}%`
   resultC.style.width = `${options[2] * 100 / total}%`;
-  resultC.innerHTML = `${(Number(options[2])*100/Number(total)).toFixed(0)}%`
+  resultC.innerHTML = `${Math.round((Number(options[2])*100/Number(total)))}%`
   resultD.style.width = `${options[3] * 100 / total}%`;
-  resultD.innerHTML = `${(Number(options[3])*100/Number(total)).toFixed(0)}%`
+  resultD.innerHTML = `${Math.round((Number(options[3])*100/Number(total)))}%`
   voters.innerHTML = `${options.reduce((acc,val)=>acc+val)}`
 }
 
-console.log(Date.now())
+const nightMode = () => {
+  if(body.style.background == "lightblue"){
+    body.style.background = "darkslategray"
+    body.style.color = "white"
+  } else {  
+    body.style.background = "lightblue"
+  }
+}
