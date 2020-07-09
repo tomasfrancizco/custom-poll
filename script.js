5a6c575a69b3ed04b37f3182b11bf4f91d5221d1
Tomas <tomasfreire@gmail.com>
2020-07-09 15:59:07 -0300

    style updates

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const resultB = document.getElementById("result-b");
 const resultC = document.getElementById("result-c");
 const resultD = document.getElementById("result-d");
 
+const voters = document.getElementById("voters");
+
 // console.log(Date.now())
 let options = [0, 0, 0, 0];
 
@@ -21,10 +23,19 @@ button.onclick = () => {
     options[2]++
   } else if(optionD.checked){
     options[3]++
+  } else {
+    return
   }
   const total = options.reduce((acc, val) => acc+val);
   resultA.style.width = `${options[0] * 100 / total}%`;
+  resultA.innerHTML = `${(Number(options[0])*100/Number(total)).toFixed(0)}%`
   resultB.style.width = `${options[1] * 100 / total}%`;
+  resultB.innerHTML = `${(Number(options[1])*100/Number(total)).toFixed(0)}%`
   resultC.style.width = `${options[2] * 100 / total}%`;
+  resultC.innerHTML = `${(Number(options[2])*100/Number(total)).toFixed(0)}%`
   resultD.style.width = `${options[3] * 100 / total}%`;
+  resultD.innerHTML = `${(Number(options[3])*100/Number(total)).toFixed(0)}%`
+  voters.innerHTML = `${options.reduce((acc,val)=>acc+val)}`
 }
+
+console.log(Date.now())
